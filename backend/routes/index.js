13dd2a1da6d02774135d5e527ea726ce31ae2ad7
Alfie2987 <125741441+Alfie2987@users.noreply.github.com>
2024-08-06 11:55:39 +0800

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.redirect('https://github.com/djcrago/blog-api');
-});
-
-module.exports = router;
